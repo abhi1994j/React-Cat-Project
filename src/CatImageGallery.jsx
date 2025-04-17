2d4c5bbce0ebe6b8fd84d9ce7937c6516e0798aa
abhi1994j <abhibaban4@gmail.com>
2025-04-17 16:09:52 +0530

    completed

---
+++ src/CatImageGallery.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import cat_1 from "./assets/cat-1.png";
+import cat_2 from "./assets/cat-2.png";
+import cat_3 from "./assets/cat-3.png";
+import cat_4 from "./assets/cat-4.png";
+
+const CatImageGallery = () => {
+  const [data, setData] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const cat = [
+    { id: 1, image: cat_1 },
+    { id: 2, image: cat_2 },
+    { id: 3, image: cat_3 },
+    { id: 4, image: cat_4 },
+  ];
+
+  function handleClick(id) {
+    const findCat = cat.find((ele) => ele.id === id);
+    console.log(findCat.image);
+    setData(findCat.image);
+    setClicked(true);
+  }
+
+  return (
+    <div className="container flex items-center flex-col">
+      <div className="w-full flex flex-wrap md:flex-nowrap justify-between items-center gap-2 p-6">
+        {cat.map((ele) => (
+          <button
+            className="max-w-xs shadow-md rounded p-1 active:border active:border-blue-400"
+            key={ele.id}
+            onClick={() => handleClick(ele.id)}
+          >
+            <img
+              className="w-full object-cover "
+              src={ele.image}
+              alt={`cat-${ele.id}`}
+            />
+          </button>
+        ))}
+      </div>
+      {clicked ? (
+        <div className="card flex max-w-sm  justify-center items-center border rounded-lg p-2 border-blue-400">
+          <img className="w-full shadow-lg object-cover" src={data} alt="" />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default CatImageGallery;
